d408959f5a461c69903ef689f1cb9dada756a288
徐梦强 <113534980@qq.com>
2024-10-21 07:44:21 +0800

    引入组件库

--- counter/src/pages/count/index.vue
+++ counter/src/pages/count/index.vue
@@ -1,41 +1,46 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+	<view class="content">
+		<image class="logo" src="/static/logo.png" />
+		<view class="text-area">
+			<text class="title">{{ title }}</text>
+		</view>
+		<wd-button>主要按钮</wd-button>
+		<wd-button type="success">成功按钮</wd-button>
+		<wd-button type="info">信息按钮</wd-button>
+		<wd-button type="warning">警告按钮</wd-button>
+		<wd-button type="error">危险按钮</wd-button>
+	</view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from 'vue';
+const title = ref('Hello');
 </script>
 
 <style>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 }
 
 .logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
 }
 
 .text-area {
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 
 .title {
-  font-size: 36rpx;
-  color: #8f8f94;
+	font-size: 36rpx;
+	color: #8f8f94;
 }
 </style>
